455c94f3c1efe228d85d06544a067d4dbc47a3f0
zhang <411152544@qq.com>
2016-06-10 15:10:18 +0800

    requestHandlers

--- node/introduce/index.js
+++ node/introduce/index.js
@@ -2,8 +2,8 @@ var server = require('./server');
 var router = require('./router');
 var requestHandlers = require('./requestHandlers');
 
-var handle = {};
-handle['/'] = requestHandlers.start;
+var handler = {};
+handler['/'] = requestHandlers.start;
 handler['/start'] = requestHandlers.start;
 handler['/upload'] = requestHandlers.upload;
 
