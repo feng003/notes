c2b154de46949efc29ce750e59e910fec200e02d
zhang <411152544@qq.com>
2016-06-27 10:42:20 +0800

    hello

--- node/express/hello/app.js
+++ node/express/hello/app.js
@@ -1,9 +1,13 @@
 var express = require('express');
-var path = require('path');
+var path    = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+var logger  = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+
+var settings = require('./settings');
+var session = require('./express-session');
+var MongoStore = require('./connect-mongo')(session);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,7 +16,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -31,6 +35,17 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    db: settings.db,
+    host: settings.host,
+    port: settings.port
+  })
+}));
 
 // error handlers
 
--- node/express/hello/routes/users.js
+++ node/express/hello/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/users/username/001',function(req,res){
+    res.send('user: ' + req.params.username);
+});
+
 module.exports = router;
